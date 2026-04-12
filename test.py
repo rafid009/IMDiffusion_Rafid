5ca75c8c1f45de65d119b29ff422b6f4ad16c83c
rafid009 <rfd.009@gmail.com>
2026-04-12 13:58:50 -0700

    Adding synthetic data processing capabilities 1-13

--- test.py
+++ test.py
@@ -1,24 +1,29 @@
 import numpy as np
 
-train_data_file = "data/swat/SWaT_minute_segments_normal.npy"#"data/synth/X_train.npy"
+train_data_file = "data/synth/X_train.npy" #"data/swat/SWaT_minute_segments_normal.npy"#
 train_data = np.load(train_data_file)
-train_data = np.nan_to_num(train_data, copy=True)
-print(f"train data: {train_data.shape}")
 
+print(f"train data: {train_data.shape}")
 train_data = train_data.reshape((-1, train_data.shape[-1]))
+mean = np.nanmean(train_data, axis=0)
+std = np.nanstd(train_data, axis=0)
+train_data = (train_data - mean) / (std + 1e-8)
+train_data = np.nan_to_num(train_data, copy=True)
 print(f"train data: {train_data.shape}")
 np.save(train_data_file, train_data)
 
-test_data_file = "data/swat/SWaT_minute_segments_anomaly.npy" #"data/synth/X_test.npy"
+test_data_file = "data/synth/X_test.npy" #"data/swat/SWaT_minute_segments_anomaly.npy"
 test_data = np.load(test_data_file)
-test_data = np.nan_to_num(test_data, copy=True)
+
 print(f"test data: {test_data.shape}")
 
 test_data = test_data.reshape((-1, test_data.shape[-1]))
+test_data = (test_data - mean) / (std + 1e-8)
+test_data = np.nan_to_num(test_data, copy=True)
 print(f"test data: {test_data.shape}")
 np.save(test_data_file, test_data)
 
-label_data_file = "data/swat/SWaT_minute_segments_anomaly_labels.npy"#"data/synth/Y_test.npy"
+label_data_file = "data/synth/Y_test.npy" #"data/swat/SWaT_minute_segments_anomaly_labels.npy"#
 label_data = np.load(label_data_file)
 
 print(f"label data: {label_data.shape}")
